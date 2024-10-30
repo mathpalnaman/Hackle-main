f36bf23e5f1aa6bdd488cfc6c7d40562979443a6
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2024-10-30 18:56:46 +0530

    a hallo

---
+++ src/app/mainGame/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hackle from '../Hackle/page'
+
+
+const mainGame = () => {
+  return (
+    <div>
+        Main Game
+        <Hackle/>
+    </div>
+  )
+}
+
+export default mainGame
